9536169e6fcd100bd849e7854adf511cb26bfdd2
Oliver Grandvuinet <og@practio.com>
2018-04-18 16:29:00 +0200

    Switch to space indentation after team vote

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 module.exports = {
-	extends: 'standard',
-	rules: {
-		'semi': ['error', 'always'],
-		'indent': ['error', 'tab', {'SwitchCase': 1}],
-		'no-tabs': 0,
-		'comma-dangle': ['error', {
-			arrays: 'always-multiline',
-			objects: 'always-multiline',
-			imports: 'always-multiline',
-			exports: 'always-multiline',
-			functions: 'always-multiline',
-		}],
-	},
+  extends: 'standard',
+  rules: {
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+  },
 };
