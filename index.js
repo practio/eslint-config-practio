ef6fed64f851b1b98b73ad7ce13a35cf5456c3e5
Oliver Grandvuinet <og@practio.com>
2019-11-07 14:52:11 +0100

    remove unnecessary rules

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 module.exports = {
   plugins: ['prettier'],
   extends: ['standard', 'plugin:prettier/recommended'],
-  rules: {
-    // 'quotes' and 'comma-dangle' must be aligned to what the config says in https://github.com/practio/prettier-config
-    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-      },
-    ],
-  },
 };
